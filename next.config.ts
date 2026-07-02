e6b0c31e3805c6ca7d3ea5c248deee84fac70c05
hannahm-26 <119318238+hannahm-26@users.noreply.github.com>
2026-07-02 21:06:02 +0200

    feat: update configuration and dependencies for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const repo = "/vacation-countdown";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: repo,
+  assetPrefix: `${repo}/`,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
